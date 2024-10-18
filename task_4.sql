bd941d4107746345a49c2181286179868fd82790
bereketa <beckyamare1@gmail.com>
2024-10-19 00:28:11 +0300

    Make task_4.sql executable

--- task_4.sql
+++ task_4.sql
@@ -1,5 +1,12 @@
 -- Select the alx_book_store database
 USE alx_book_store;
 
--- Show the full structure of the Books table
-SHOW COLUMNS FROM Books;
+-- Retrieve the full description of the Books table
+SELECT 
+    COLUMN_NAME, 
+    COLUMN_TYPE 
+FROM 
+    INFORMATION_SCHEMA.COLUMNS 
+WHERE 
+    TABLE_SCHEMA = 'alx_book_store' 
+    AND TABLE_NAME = 'Books';
